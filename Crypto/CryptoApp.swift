0402a7a0d538ccfb12db5aaacf07d13451d374b3
John Reichel <jreichel447@gmail.com>
2023-01-25 16:29:54 -0600

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by John Reichel on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
